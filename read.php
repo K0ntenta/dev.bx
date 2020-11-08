df5e5315850f0e21f2faa106f7a26851a3e0b67a
Гаврилюк Михаил <gavrik1998@list.ru>
2020-11-08 21:53:49 +0200

    Задание 2

---
+++ read.php
@@ -0,0 +1,19 @@
+<?php
+
+function readFromConsole($input){
+    if ($input=='true'){
+        return 'true';
+    }
+    if ($input=='false'){
+        return 'false';
+    }
+    if ($input=='!stop'){
+        return 'null';
+    }
+    if (is_numeric($input)){
+        return $input;
+    }
+    if (is_string($input)){
+        return $input;
+    }
+}
