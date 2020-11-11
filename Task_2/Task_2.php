2a38f3e9f6d67e87ceb513b0e3052bb694967c21
Гаврилюк Михаил <gavrik1998@list.ru>
2020-11-11 16:29:47 +0200

    Задание 2

--- Task_2/Task_2.php
+++ Task_2/Task_2.php
@@ -2,48 +2,52 @@
 
 require('read_2.php');
 
-echo 'Тесты:'.PHP_EOL;
+echo 'Тесты:' . PHP_EOL;
 readFromConsoleTest();
 
-echo PHP_EOL.'Введите данные для проверки: '.PHP_EOL;
-$funcResult=readFromConsole();
+echo PHP_EOL . 'Введите данные для проверки: ' . PHP_EOL;
+$funcResult = readFromConsole();
 
-echo 'Ожидаемый результат: '.PHP_EOL;
-$myResult=trim(fgets(STDIN));
+echo 'Ожидаемый результат: ' . PHP_EOL;
+$myResult = trim(fgets(STDIN));
 
-function assertEquals($myResult,$funcResult):string{
-    if ($myResult==$funcResult){
+function assertEquals($myResult, $funcResult):string
+{
+    if ($funcResult == $myResult)
+    {
         return 'test passed';
     }
-    else{
+    else
+    {
         return 'test failed';
     }
 }
 
-echo 'Результат: '.assertEquals($myResult,$funcResult).PHP_EOL;
+echo 'Результат: ' . assertEquals($myResult, $funcResult) . PHP_EOL;
 
-function readFromConsoleTest(){
-    $result=readFromConsole('true');
-    echo '1) Результат функции: '.$result.PHP_EOL.' Ожидаемый результат: true'.PHP_EOL;
-    echo 'Ожидание/реальность?'.($result==='true'? ' Да'.PHP_EOL:' Нет'.PHP_EOL);
+function readFromConsoleTest()
+{
+    $result = readFromConsole('true');
+    echo '1) Результат функции: ' . $result . PHP_EOL . ' Ожидаемый результат: true' . PHP_EOL;
+    echo 'Ожидание/реальность?' . ($result === true ? ' Да' . PHP_EOL : ' Нет' . PHP_EOL);
 
-    $result=readFromConsole('false');
-    echo '2) Результат функции: '.$result.PHP_EOL.' Ожидаемый результат: false'.PHP_EOL;
-    echo 'Ожидание/реальность?'.($result==='false'? ' Да'.PHP_EOL:' Нет'.PHP_EOL);
+    $result = readFromConsole('false');
+    echo '2) Результат функции: ' . $result . PHP_EOL . ' Ожидаемый результат: false' . PHP_EOL;
+    echo 'Ожидание/реальность?' . ($result === false ? ' Да' . PHP_EOL : ' Нет' . PHP_EOL);
 
-    $result=readFromConsole('!stop');
-    echo '3) Результат функции: '.$result.PHP_EOL.' Ожидаемый результат: null'.PHP_EOL;
-    echo 'Ожидание/реальность?'.($result==='null'? ' Да'.PHP_EOL:' Нет'.PHP_EOL);
+    $result = readFromConsole('!stop');
+    echo '3) Результат функции: ' . $result . PHP_EOL . ' Ожидаемый результат: null' . PHP_EOL;
+    echo 'Ожидание/реальность?' . ($result === null ? ' Да' . PHP_EOL : ' Нет' . PHP_EOL);
 
-    $result=readFromConsole('1.3');
-    echo '4) Результат функции: '.$result.PHP_EOL.' Ожидаемый результат: 1.3'.PHP_EOL;
-    echo 'Ожидание/реальность?'.($result==='1.3'? ' Да'.PHP_EOL:' Нет'.PHP_EOL);
+    $result = readFromConsole('1.3');
+    echo '4) Результат функции: ' . $result . PHP_EOL . ' Ожидаемый результат: 1.3' . PHP_EOL;
+    echo 'Ожидание/реальность?' . ($result === 1.3 ? ' Да' . PHP_EOL : ' Нет' . PHP_EOL);
 
     $result=readFromConsole('1');
-    echo '5) Результат функции: '.$result.PHP_EOL.' Ожидаемый результат: 1'.PHP_EOL;
-    echo 'Ожидание/реальность?'.($result==='1'? ' Да'.PHP_EOL:' Нет'.PHP_EOL);
+    echo '5) Результат функции: ' . $result . PHP_EOL . ' Ожидаемый результат: 1' . PHP_EOL;
+    echo 'Ожидание/реальность?' . ($result === 1 ? ' Да' . PHP_EOL : ' Нет' . PHP_EOL);
 
     $result=readFromConsole('test');
-    echo '6) Результат функции: '.$result.PHP_EOL.' Ожидаемый результат: test'.PHP_EOL;
-    echo 'Ожидание/реальность?'.($result==='test'? ' Да'.PHP_EOL:' Нет'.PHP_EOL);
+    echo '6) Результат функции: ' . $result . PHP_EOL . ' Ожидаемый результат: test' . PHP_EOL;
+    echo 'Ожидание/реальность?' . ($result === 'test' ? ' Да' . PHP_EOL : ' Нет' . PHP_EOL);
 }
